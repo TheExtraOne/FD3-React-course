613646fa769f412fb1b29fc4b662139dd29e6f82
TheExtraOne <katya_mihasewa@mail.ru>
2022-11-05 20:19:39 +0300

    refactor: and another solution

--- rainbowframe/src/components/rainbow-frame.js
+++ rainbowframe/src/components/rainbow-frame.js
@@ -1,31 +1,43 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import './rainbow-frame.css';
 
 class RainbowFrame extends React.Component {
-    
-    state = {
-        colors: this.props.colors.slice(0, this.props.colors.length-1),
-        children: this.props.children
-    };
 
     render() {
-        if (this.props.colors.length > 1) {
-            return (
-                <div style={{border:"solid 10px "+ this.props.colors[this.props.colors.length - 1], padding:"10px"}}>
-                    <RainbowFrame colors={this.state.colors} children={this.state.children}/>
-                </div>
-            );
-        } else {
-            return (
-                <div style={{border:"solid 10px "+ this.props.colors[this.props.colors.length - 1], padding:"10px"}}>
-                    {this.state.children}
-                </div>
-            );
-        }
-        
+        let tag = <div style={{border:"solid 10px "+ this.props.colors[0], padding:"10px"}}>{this.props.children}</div>;
+
+        let createRecurTag = (color) => {tag = <div style={{border:"solid 10px "+ color, padding:"10px"}}>{tag}</div>};
+
+        this.props.colors.forEach((color) => createRecurTag(color));
+
+        return (tag);
     }
 }
+
+// class RainbowFrame extends React.Component {
+    
+//     state = {
+//         colors: this.props.colors.slice(0, this.props.colors.length-1),
+//         children: this.props.children
+//     };
+
+//     render() {
+//         if (this.props.colors.length > 1) {
+//             return (
+//                 <div style={{border:"solid 10px "+ this.props.colors[this.props.colors.length - 1], padding:"10px"}}>
+//                     <RainbowFrame colors={this.state.colors} children={this.state.children}/>
+//                 </div>
+//             );
+//         } else {
+//             return (
+//                 <div style={{border:"solid 10px "+ this.props.colors[this.props.colors.length - 1], padding:"10px"}}>
+//                     {this.state.children}
+//                 </div>
+//             );
+//         }
+        
+//     }
+// }
 
 export default RainbowFrame;
